e35503566817e81ca726722c039cb1e3b5758d46
Devany <devanydc>
2019-09-23 20:36:28 +0700

    Half of Bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
@@ -6,12 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class BundleActivity extends AppCompatActivity {
+
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY ="name";
+    public static final String AGE_KEY="age";
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bundle);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
     }
+
+    public void handleSubmit(View view) {
+        Intent intent= new Intent(this,ProfileBundleActivity.class);
+        String username =usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
+
+        usernameText.setText(username);
+        nameText.setText(name);
+        ageText.setText(age);
+        startActivity(intent);
+
+    }
+
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,19 +3,37 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY ="name";
+    public static final String AGE_KEY="age";
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
 
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            String username = extras.getString(USERNAME_KEY);
+            String name = extras.getString(NAME_KEY);
+            int age = extras.getInt(AGE_KEY);
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(age);
         }
     }
 }
