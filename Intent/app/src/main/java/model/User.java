d498be3696cf9e0062116929550640df49ca674b
Devany <devanydc>
2019-09-24 23:43:27 +0700

    User.java half done

--- Intent/app/src/main/java/model/User.java
+++ Intent/app/src/main/java/model/User.java
@@ -1,14 +1,22 @@
 package model;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
     private String username;
     private String name;
     private int age;
 
+    public User(String username, String name,int age){
+        this.username=username;
+        this.name=name;
+        this.age=age;
+    }
+
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
@@ -16,7 +24,6 @@ public class User {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -24,10 +31,45 @@ public class User {
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(this.username);
+        dest.writeString(this.name);
+        dest.writeInt(this.age);
+    }
+
+    protected User(Parcel in){
+        this.username= in.readString();
+        this.name= in.readString();
+        this.age= in.readInt();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel parcel) {
+            @Override
+                    public User createFromParcel(Parcel source){
+                return new User(source);
+            }
+
+            @Override
+                    public User[] newArray(int size){
+                return new User(size);
+            }
+        }
+
+        @Override
+        public User[] newArray(int i) {
+            return new User[0];
+        }
+    }
 }
