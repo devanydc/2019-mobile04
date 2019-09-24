64b12159995538030d5097c26964df27bfd5e78f
Devany <devanydc>
2019-09-24 22:09:38 +0700

    Getter and Setter

--- Intent/app/src/main/java/model/User.java
+++ Intent/app/src/main/java/model/User.java
@@ -1,4 +1,33 @@
 package model;
 
 public class User {
+    private String username;
+    private String name;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
